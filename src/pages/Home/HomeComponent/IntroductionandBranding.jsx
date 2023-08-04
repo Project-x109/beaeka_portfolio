c2087e59558014e5c93cac0ee84c4ca8908ee422
Project-x109 <amanuelgirma109@gmail.com>
2023-08-04 11:51:27 +0300

    minor

--- src/pages/Home/HomeComponent/IntroductionandBranding.jsx
+++ src/pages/Home/HomeComponent/IntroductionandBranding.jsx
@@ -1,23 +1,15 @@
 // src/pages/Home.js
 
 import React from "react";
-import "./IntroductionandBranding.css"
+import "./IntroductionandBranding.css";
 const IntroductionandBranding = () => {
   return (
     <div>
       <section class="intro-section">
         <div class="container">
           <div class="intro-content">
-            <h1 class="company-name">Your Company Name</h1>
-            <h2 class="tagline">Tagline or Catchy Phrase Here</h2>
-            <p class="key-messages">
-              Welcome to our portfolio! We are a creative team passionate about
-              delivering high-quality products and services. Our mission is to
-              make a positive impact through innovative solutions.
-            </p>
-            <a href="#portfolio" class="cta-button">
-              Explore Our Work
-            </a>
+            <h1 class="company-name">WELCOME TO </h1>
+            <h1 class="company-name">BEAEKA General Business</h1>
           </div>
         </div>
       </section>
