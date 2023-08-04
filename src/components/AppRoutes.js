557a70e717436a0d3c97f10c9dfbae3cfc55973b
Project-x109 <amanuelgirma109@gmail.com>
2023-08-04 09:11:10 +0300

    minor

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
+import Services from "../pages/Services/Services";
 function AppRouter() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
       {/* Add more routes for other pages */}
     </Routes>
     </BrowserRouter>
---
+++ src/pages/About/About.jsx
@@ -0,0 +1,13 @@
+// src/pages/Home.js
+
+import React from 'react';
+const About = () =>{
+  return (
+    <div>
+      <h1>About Page</h1>
+      {/* Add content for your home page */}
+    </div>
+  );
+}
+
+export default About;
